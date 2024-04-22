619be6f03dfd2cc20917418cbcaa043384465ece
kazuuoooo <kazkte@gmail.com>
2024-04-22 11:05:51 -0700

    new internship postings

--- data/create_read_me.py
+++ data/create_read_me.py
@@ -44,7 +44,7 @@ for listing in listings[:500]:
     apply_url = listing.get('Apply', '')
     apply_url_encoded = urllib.parse.quote(apply_url, safe=':/')
     link_button = (f'<a href="{apply_url_encoded}"><img src="data/images/applybutton.png" alt="Apply Button" style="width:85px;"></a>'
-                    f'<a href="https://www.ribbon.ai/install"><img src="data/images/ribbonsmall.png" alt="Ribbon" style="width:85px;"></a>')
+                    f'<a href="https://www.ribbon.ai/install"><img src="data/images/ribbonbutton.png" alt="Ribbon" style="width:85px;"></a>')
 
     markdown_content += f"| {company_name} | {role} | {locations} | {link_button} | {date_posted} |\n"
 
